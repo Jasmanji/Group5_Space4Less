c38a0376fc05b4801ae252bc8b9db05699ddab6d
Kowther <kowtheri@hotmail.co.uk>
2020-02-17 18:30:29 +0000

    basic forms (no styling)

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,19 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                          validators=[DataRequired(), Length(min=2, max=20)]
+                           validators=[DataRequired(), Length(min=4, max=20)]
+                           )
+    firstname = StringField('First Name',
+                            validators=[DataRequired()]
+                            )
+    surname = StringField('Surname',
+                          validators=[DataRequired()]
                           )
     email = StringField('Email',
                         validators=[DataRequired(), Email()]
                         )
     password = PasswordField('Password',
-                            validators=[DataRequired()]
-                            )
+                             validators=[DataRequired()]
+                             )
     confirm_password = PasswordField('Confirm Password',
-                                    validators=[DataRequired(), EqualTo('password')]
-                                    )
+                                     validators=[DataRequired(), EqualTo('password')]
+                                     )
     submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()]
+                        )
+    password = PasswordField('Password',
+                             validators=[DataRequired()]
+                             )
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Log In')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, flash, redirect, url_for, Blueprint, request
+from sqlalchemy.exc import IntegrityError
+
 # the render template is to help us with returning an html template for a route
 # the url_for is a function within flask that will find the exact location of routes for us
 from app import RegistrationForm, db
+from app.main.forms import LoginForm
 from app.models import User
-from sqlalchemy.exc import IntegrityError
 
 # we create an instance of blueprint as main
 bp_main = Blueprint('main', __name__)
+
 
 # route for home page.
 @bp_main.route('/')
@@ -14,11 +17,12 @@ bp_main = Blueprint('main', __name__)
 def home_page():
     return render_template('home.html')
 
+
 # route for signup page
 @bp_main.route("/signup", methods=['GET', 'POST'])
 def signup_page():
     form = RegistrationForm()
-    if form.validate_on_submit() and request.method=='POST':  # this will tell us if the for was valid when submitted
+    if form.validate_on_submit() and request.method == 'POST':  # this will tell us if the for was valid when submitted
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         try:
@@ -31,13 +35,14 @@ def signup_page():
             flash('there was something wrong with your password or email!', 'error')
     return render_template('signup.html', title='signup', form=form)
 
+
 # @bp_main.route('/blog', methods=['GET'])
 # def blog_page():
 #     forecast_info = Forecast.query.all()
 #     return render_template('blogs.html', title='blog posts', forecast=forecast_info)
 
 
-# @app.route('/login')
-# def login():
-#     form_instance=LoginForm()
-#     return render_template('login.html', title='login', form=form_instance)
+@bp_main.route("/login", methods=['GET', 'POST'])
+def login():
+    form_instance = LoginForm()
+    return render_template('login.html', title='login', form=form_instance)
